abc438bb9a5477218a6b043831d477eca2078564
Alexandre <overed64@gmail.com>
2021-04-23 23:36:34 +0200

    GenerateHTML
    Done

--- src/component/output/Html.java
+++ src/component/output/Html.java
@@ -5,196 +5,239 @@ import model.Student;
 import utils.PopupManager;
 
 import java.io.FileWriter;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Html implements Generator {
+
+    private static final String HTML_DOC =
+            """
+                    <!DOCTYPE html>
+                    <html lang="fr">
+                    <head>
+                        <meta charset="utf-8">
+                        <title> Attendance Report </title>
+                        <link rel="stylesheet" media="all" href="visu.css">
+                    </head>
+                    <style>
+                        body{
+                            background-color: #ddd;
+                            font-family: Verdana, sans-serif;
+                        }
+                        #people {
+                            width: 1230px;
+                            background-color: #fff;
+                            margin-bottom: 30px;
+                            padding: 10px 0;
+                        }
+                        th {
+                            text-align: left;
+                            padding-right: 10px;
+                        }
+                        p {
+                            width:1230px;
+                        }
+                        .data {
+                            display: grid;
+                            grid-column-gap: 5px;
+                            grid-template-areas: "col0 col1 col2 col3 col4";
+                            height:20px;
+                            margin:0 10px;
+                            border: solid 1px #fff;
+                        }
+                        .data:hover {
+                            border: solid 1px red;
+                        }
+                        .id {
+                            grid-area: col0;
+                            width : 100px;
+                            white-space: nowrap;
+                            padding-left: 3px;
+                        }
+                        .name {
+                            grid-area: col1;
+                            width : 200px;
+                            text-overflow: ellipsis;
+                            white-space: nowrap;
+                            padding-left: 3px;
+                        }
+                        .duration {
+                            grid-area: col3;
+                            text-align: center;
+                            width: 40px;
+                            background-color: #ddd;
+                        }
+                        .percent {
+                            grid-area: col4;
+                            text-align: center;
+                            width: 60px;
+                            background-color: #ddd;
+                        }
+                        .timeBar {
+                            grid-area: col2;
+                            width: 780px;
+                            background-color: #DDD;
+                            border-left: solid 2px black;
+                            border-right: solid 2px black;
+                        }
+                        .timeBar > div {
+                            padding: 0;
+                            margin: 0;
+                            height: 20px;
+                            float:left;
+                        }
+                        .name, .duration, .percent, .id {
+                            vertical-align: middle;
+                            line-height: 20px;
+                        }
+                        .green{ background-color: #00FF4C; }
+                        .white{ background-color:white; }
+                    </style>
+                    <body>
+                        <h1> Rapport de connexion </h1>
+                        <div>
+                            <table>
+                                <tr>
+                                    <th> Date : </th>
+                                    <td> %s </td>
+                                </tr>
+                                <tr>
+                                    <th> Heure début : </th>
+                                    <td> %s </td>
+                                </tr>
+                                <tr>
+                                    <th> Heure fin : </th>
+                                    <td> %s </td>
+                                </tr>
+                                <tr>
+                                    <th> Cours : </th>
+                                    <td> %s </td>
+                                </tr>
+                                <tr>
+                                    <th> Fichier analysé : </th>
+                                    <td> %s </td>
+                                </tr>
+                                <tr>
+                                    <th> Nombre de connectés : </th>
+                                    <td> %s  </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <h2> Durées de connexion</h2>
+                        <p>
+                            Pour chaque personne ci-dessous, on retrouve son temps total de connexion sur la plage déclarée du cours, ainsi
+                            qu'un graphe qui indique les périodes de connexion (en vert) et d'absence de connexion (en blanc). En pointant
+                            la souris sur une zone, une bulle affiche les instants de début et de fin de période.
+                        </p>
+                        <div id="people">
+                            %s
+                        </div>
+                    </body>
+                    </html>
+                    """;
+
+    private static final String HTML_DATA_BLOCK =
+            """
+                    <div class="data">
+                        <div class="id"> %s </div>
+                        <div class="name"> %s </div>
+                        <div class="timeBar">
+                            %s
+                        </div>
+                        <div class="duration"> %s </div>
+                        <div class="percent"> %s </div>
+                    </div>
+                    """;
+
+    private static final String TIME_BAR_ITEM =
+            """
+                    <div class="%s" style="width:%s" title="%s"></div>
+                    """;
+
     @Override
-    public void Generate( Classroom classroom )
-    {
-        try
-        {
-            FileWriter html = new FileWriter(classroom.getName() + ".html");
-            html.append("<!DOCTYPE html>\n");
-            html.append("   <html lang=\"fr\">\n");
-            html.append("   <head>\n");
-            html.append("       <meta charset=\"utf-8\"><title> Attendance Report </title>\n");
-            html.append("       <link rel=\"stylesheet\" media=\"all\" href=\"visu.css\">\n");
-            html.append("   </head>\n");
-            html.append("   <body>\n");
-            html.append("       <h1> Rapport de connexion </h1>\n\n");
-            html.append("       <div id=\"blockid\">");
-            html.append("       <table>\n");
-            html.append("           <tr>\n");
-            html.append("               <th> Date : </th>\n");
-            html.append("               <td>" + classroom.getDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + "</td>\n");
-            html.append("           </tr>\n");
-            html.append("           <tr>\n");
-            html.append("               <th> Heure début : </th>\n");
-            html.append("               <td> " + classroom.getBegin().toLocalTime() + "</td>\n");
-            html.append("           </tr>\n");
-            html.append("           <tr>\n");
-            html.append("               <th> Heure fin : </th>\n");
-            html.append("               <td>" + classroom.getEnd().toLocalTime() +"</td>\n");
-            html.append("           </tr>\n");
-            html.append("           <tr>\n");
-            html.append("               <th> Cours : </th>\n");
-            html.append("               <td>" + classroom.getName() + "</td>\n");
-            html.append("           </tr>\n");
-            html.append("           <tr>\n");
-            html.append("               <th> Fichier analysé : </th>\n");
-            html.append("               <td>" + classroom.getFilename() + "</td>\n");
-            html.append("           </tr>\n");
-            html.append("           <tr>\n");
-            html.append("               <th> Nombre de connectés : </th>\n");
-            html.append("               <td>" + classroom.getStudents().size() + "</td>\n");
-            html.append("           </tr>\n");
-            html.append("       </table>\n\n");
-            html.append("       <h2> Durées de connexion</h2>\n\n");
-            html.append("       <p> Pour chaque personne ci-dessous, on retrouve son temps total de connexion sur la plage déclarée du cours, ainsi qu'un graphe qui indique les périodes de connexion (en vert) et d'absence de connexion (en blanc). En pointant la souris sur une zone, une bulle affiche les instants de début et de fin de période.</p>\n");
-            html.append("       <div id=\"blockpeople\">\n");
+    public void Generate( Classroom classroom ) {
+        try {
+            FileWriter html = new FileWriter( classroom.getName() + ".html" );
 
-            for (Student student : classroom.getStudents())
-            {
-                html.append(        "<div class=\"datapeople\">\n");
-                html.append(        "<div class=\"name\">" + student.getName() + "</div>\n");
-                html.append(        "<div class=\"timebar\">\n");
+            String date = classroom.getDate().format( DateTimeFormatter.ofPattern( "dd/MM/yyyy" ) );
+            String begin = classroom.getBegin().toLocalTime().toString();
+            String end = classroom.getEnd().toLocalTime().toString();
+            String name = classroom.getName();
+            String fileName = classroom.getFilename();
+            String studentAmount = Integer.toString( classroom.getStudents().size() );
+            StringBuilder data = new StringBuilder();
 
-                for (int i = 0; i < student.getEventList().size(); i = i+2)
-                {
+            long classroomDuration = Duration.between( classroom.getBegin() , classroom.getEnd() ).toSeconds();
 
+            for ( Student student : classroom.getStudents() ) {
+                String id = student.getId();
+                String identity = student.getName();
+                String totalAttendance = Long.toString( student.getTotalAttendanceDuration() );
+                String percentAttendance = Integer.toString( student.getAttendancePercent( classroom.getDureeCours() )) + "%";
+                StringBuilder timeBar = new StringBuilder();
+
+                LocalDateTime blockBegin = null;
+                LocalDateTime lastEnd = classroom.getBegin();
+                boolean isOpen = false;
+                for ( LocalDateTime event : student.getEventList() ) {
+                    if( !isOpen ) {
+                        blockBegin = event;
+                        if( !lastEnd.equals(blockBegin) ){
+                            timeBar.append(generateTimeBarDiv(lastEnd,blockBegin,classroomDuration,false));
+                        }
+                    } else {
+                        timeBar.append(generateTimeBarDiv(blockBegin,event,classroomDuration,true));
+                        lastEnd = event;
+                    }
+                    isOpen = !isOpen;
                 }
 
-                html.append("       </div>\n");
-                html.append("       <div class=\"duration\">" + student.getTotalAttendanceDuration() + "</div> \n");
-                html.append("       <div class=\"percentd\"> 100% </div> \n");
-                html.append("       </div>\n");
+                if( !lastEnd.equals(classroom.getEnd()) ){
+                    timeBar.append(generateTimeBarDiv(lastEnd,classroom.getEnd(),classroomDuration,false));
+                }
 
+                String studentData = String.format(
+                        HTML_DATA_BLOCK,
+                        id,
+                        identity,
+                        timeBar,
+                        totalAttendance,
+                        percentAttendance
+                        );
+
+                data.append( studentData );
             }
 
-            html.append("       </div>\n");
-            html.append("   </body> \n");
-            html.append("</html>\n");
+            String fullPage = String.format(
+                    HTML_DOC,
+                    date,
+                    begin,
+                    end,
+                    name,
+                    fileName,
+                    studentAmount,
+                    data
+            );
 
+            html.append( fullPage );
             html.flush();
             html.close();
-        }
-        catch (Exception e)
-        {
-            PopupManager.showAlert("Erreur lors de la création du fichier !");
+        } catch ( Exception e ) {
+            PopupManager.showAlert( "Erreur lors de la création du fichier !" );
         }
     }
 
-    /*
-
-============================[ FROM PROCESSOR ]===============================
-
-public String toHTMLCode() {
-
-        String html = "<!DOCTYPE html> \n <html lang=\"fr\"> \n <head> \n <meta charset=\"utf-8\"> ";
-        html += "<title> Attendance Report </title> \n <link rel=\"stylesheet\" media=\"all\" href=\"visu.css\"> \n";
-        html += "</head> \n <body> \n ";
-        html += "<h1> Rapport de connexion </h1>\n" +
-                "\n" +
-                "<div id=\"blockid\">\n" +
-                "<table>\n" +
-                "\t<tr>\n" +
-                "\t\t<th> Date : </th>\n" +
-                "\t\t<td> " + this._allpeople.iterator().next().getDate() + " </td>\n" +
-            "\t</tr>\n" +
-            "\t<tr>\n" +
-            "\t\t<th> Heure début : </th>\n" +
-            "\t\t<td> " + this._startTime + " </td>\n" +
-            "\t</tr>\n" +
-            "\t<tr>\n" +
-            "\t\t<th> Heure fin : </th>\n" +
-            "\t\t<td> " + this._endTime + " </td>\n" +
-            "\t</tr>\n" +
-            "\t<tr>\n" +
-            "\t\t<th> Cours : </th>\n" +
-            "\t\t<td> CM Bases de données et programmation Web </td>\n" +
-            "\t</tr>\n" +
-            "\t<tr>\n" +
-            "\t\t<th> Fichier analysé : </th>\n" +
-            "\t\t<td> " + this._fileName + " </td>\n" +
-            "\t</tr>\n" +
-            "\t<tr>\n" +
-            "\t\t<th> Nombre de connectés : </th>\n" +
-            "\t\t<td> " + this._allpeople.size() + "  </td>\n" +
-            "\t</tr>\n" +
-            "</table>\n" +
-            "</div>\n" +
-            "\n" +
-            "<h2> Durées de connexion</h2>\n" +
-            "\n" +
-            "<p> Pour chaque personne ci-dessous, on retrouve son temps total de connexion sur la plage déclarée du classroom, ainsi qu'un graphe qui indique les périodes de connexion (en vert) et d'absence de connexion (en blanc). En pointant la souris sur une zone, une bulle affiche les instants de début et de fin de période. \n" +
-            "</p>";
-    html += "<div id=\"blockpeople\"> ";
-
-        for ( Student student : this._allpeople) {
-
-        html += student.getHTMLCode();
+    private String generateTimeBarDiv(LocalDateTime begin,LocalDateTime end,long total, boolean isConnected) {
+        long duration = Duration.between( begin , end ).toSeconds();
+        double percent = (double)duration * 100 / (double)total;
+        return String.format(
+                TIME_BAR_ITEM,
+                (isConnected)?"green":"white",
+                percent + "%",
+                (isConnected)?
+                        "connecté(e) de " + begin.toLocalTime().toString() + " à " + end.toLocalTime().toString():
+                        "déconnecté(e) de " + begin.toLocalTime().toString() + " à " + end.toLocalTime().toString()
+        );
     }
-
-    html += "</div> \n </body> \n </html>";
-        return html;
-}
-
-    ============================[ FROM STUDENT ]===============================
-
-    public String getHTMLCode() {
-
-        if ( this.isOutOfPeriod() ) return ("");
-
-        // duration max, in order to compute images width in percent
-        LocalDateTime startTime = DateTimeConverter.StringToLocalDateTime(this._start);
-        LocalDateTime endTime = DateTimeConverter.StringToLocalDateTime(this._stop);
-        Duration delayMax = Duration.between(startTime, endTime);
-        double durationMaxMinutes = Math.abs(delayMax.toSeconds()/60.);
-
-        String html="";
-		html += "<div class=\"datapeople\"> \n";
-		html += "<div class=\"name\"> " + this.getName() + " </div> \n";
-		html +=	"<div class=\"timebar\">";
-
-        double totalDuration = 0;
-        LocalDateTime refTime = DateTimeConverter.StringToLocalDateTime(this._start);
-        for ( Period period : this._periodList) {
-
-            LocalDateTime begin = period.get_start();
-            LocalDateTime end = period.get_end();
-            double duration = period.getDurationInMinutes();
-            totalDuration += duration;
-            // begin > reftime : white bar
-            Duration delay = Duration.between(refTime, begin);
-            double delayMinutes = Math.abs(delay.toSeconds()/60.);
-            if (delayMinutes>0.0) {
-                html += "<fxml.img src=\"off.png\" ";
-                html += "width=\"" + (100.*delayMinutes/durationMaxMinutes) + "%\" ";
-                html += "height=\"20\" title=\"absent(e) de " + refTime.toString();
-                html += " à " + begin.toString() + " \"> \n";
-            }
-            // green bar for the current period
-            html += "<fxml.img src=\"on.png\" ";
-            html += "width=\"" + (100.*duration/durationMaxMinutes) + "%\" ";
-            html += "height=\"20\" title=\"connecté(e) de " + begin.toString();
-            html += " à " + end.toString()+ "\"> \n";
-            refTime = end;
-        }
-        // last period aligned on end time ?
-        //LocalDateTime endTime = component.teams.TEAMSDateTimeConverter.StringToLocalDateTime(this._stop);
-        Duration delay = Duration.between(refTime, endTime);
-        double delayMinutes = Math.abs(delay.toSeconds()/60.);
-        if (delayMinutes>0.0) {
-            html += "<fxml.img src=\"off.png\" ";
-            html += "width=\"" + (100.*delayMinutes/durationMaxMinutes) + "%\" ";
-            html += "height=\"20\" title=\"absent(e) de " + refTime.toString();
-            html += " à " + endTime.toString() + " \"> \n";
-        }
-		html += "</div> \n"; // end of div timebar
-        html +=	"<div class=\"duration\"> " + (long)Math.round(totalDuration) + " </div> \n";
-        html +=	"<div class=\"percentd\"> " + (long)Math.round(100.*totalDuration/durationMaxMinutes) + "% </div> \n";
-        html += "</div>\n"; // end of div datapeople
-        return html;
-    }
-    */
 }
