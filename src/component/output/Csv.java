9e182d1d4dc37a56a0b792fb6aac462ff7a81194
Adam <adam.henault@etu.univ-tours.fr>
2021-04-23 19:09:41 +0200

    Update Csv.java

--- src/component/output/Csv.java
+++ src/component/output/Csv.java
@@ -1,10 +1,35 @@
 package component.output;
 
 import model.Classroom;
+import model.Student;
+import utils.PopupManager;
 
-public class Csv implements Generator {
+import java.io.FileWriter;
+
+public class Csv implements Generator
+{
     @Override
-    public void Generate( Classroom classroom ) {
-
+    public void Generate( Classroom classroom )
+    {
+        try
+        {
+            FileWriter csv = new FileWriter(classroom.getName() + ".csv");
+            csv.append("ID" + ";" + "Nom" + ";" + "Temps de connexion" + "\n");
+            for (Student student : classroom.getStudents())
+            {
+                csv.append(student.getId());
+                csv.append(";");
+                csv.append(student.getName());
+                csv.append(";");
+                csv.append(String.valueOf(student.getTotalAttendanceDuration()));
+                csv.append("\n");
+            }
+            csv.flush();
+            csv.close();
+        }
+        catch (Exception e)
+        {
+            PopupManager.showAlert("Erreur lors de la création du fichier !");
+        }
     }
 }
